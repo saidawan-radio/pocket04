4ea068c20b4e438618c17405ce9f794cde6366c2
Mohsen Mohamadian <moohamadiann@gmail.com>
2026-06-26 02:18:02 +0330

    set default value when ENV not access

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class conf:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    SESSION_STR= os.getenv("SESSION_STR")
-    CHANNEL_USERNAME= os.getenv("CHANNEL_USERNAME")
-    DOWNLOAD_PATH=os.getenv("DOWNLOAD_PATH")
-    ORIGIN_DATA_FILE_PATH = os.getenv("ORIGIN_DATA_FILE_PATH")
-    DOWNLOADED_DATA_FILE_PATH = os.getenv("DOWNLOADED_DATA_FILE_PATH")
-    DATE_FORMAT = os.getenv("DATE_FORMAT")
-    LOAD_START_DATE = os.getenv("LOAD_START_DATE")
-    DURATION_LIMIT = int(os.getenv("DURATION_LIMIT"))
-    MIN_MSG_ID = int(os.getenv("MIN_MSG_ID"))
-    DATA_FETCH_LIMIT = int(os.getenv("DATA_FETCH_LIMIT"))
-    DATA_FETCH_SIZE_LIMIT = os.getenv("DATA_FETCH_SIZE_LIMIT")
-    FILENAME_PATTERN = os.getenv("FILENAME_PATTERN")
+    API_ID = int(os.getenv("API_ID", 0))
+    API_HASH = os.getenv("API_HASH", 0)
+    SESSION_STR= os.getenv("SESSION_STR", 0)
+    CHANNEL_USERNAME= os.getenv("CHANNEL_USERNAME", 0)
+    DOWNLOAD_PATH=os.getenv("DOWNLOAD_PATH", 0)
+    ORIGIN_DATA_FILE_PATH = os.getenv("ORIGIN_DATA_FILE_PATH", 0)
+    DOWNLOADED_DATA_FILE_PATH = os.getenv("DOWNLOADED_DATA_FILE_PATH", 0)
+    DATE_FORMAT = os.getenv("DATE_FORMAT", 0)
+    LOAD_START_DATE = os.getenv("LOAD_START_DATE", 0)
+    DURATION_LIMIT = int(os.getenv("DURATION_LIMIT", 0))
+    MIN_MSG_ID = int(os.getenv("MIN_MSG_ID", 0))
+    DATA_FETCH_LIMIT = int(os.getenv("DATA_FETCH_LIMIT", 0))
+    DATA_FETCH_SIZE_LIMIT = os.getenv("DATA_FETCH_SIZE_LIMIT", 0)
+    FILENAME_PATTERN = os.getenv("FILENAME_PATTERN", 0)
     DOWNLOADED_JSON_DATA_FORM = {
                 "audio_info" : {},
                 "map_msg_id" : {},
@@ -26,6 +26,6 @@ class conf:
                 }
             }
     
-    REPO_OWNER = os.getenv("REPO_OWNER")
-    REPO_NAME = os.getenv("REPO_NAME")
-    REPO_BRANCH = os.getenv("REPO_BRANCH")
+    REPO_OWNER = os.getenv("REPO_OWNER", 0)
+    REPO_NAME = os.getenv("REPO_NAME", 0)
+    REPO_BRANCH = os.getenv("REPO_BRANCH", 0)
